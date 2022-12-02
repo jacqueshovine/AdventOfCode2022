2f866222f93339ba52b2e26cd2c6a736d3813725
Jacques Hovine <jacques.hovine@partoo.fr>
2022-12-02 16:23:38 +0100

    day01 part 2

--- day01/day01.py
+++ day01/day01.py
@@ -1,18 +1,41 @@
+# Part 1
+
+# with open("../inputs/day01.csv") as f:
+
+#     total = 0
+#     highest = 0
+
+#     for line in f:
+#         if line == '\n':
+#             if total > highest:
+#                 highest = total
+
+#             # Reseting total value after each linebreak
+#             total = 0
+#         else:
+#             total += int(line)
+
+# print("@@@@@@ OUTPUT @@@@@@")
+# print(highest)
+
+# Part 2
 
 with open("../inputs/day01.csv") as f:
 
+    calories = []
     total = 0
-    highest = 0
 
     for line in f:
         if line == '\n':
-            if total > highest:
-                highest = total
+            calories.append(total)
 
             # Reseting total value after each linebreak
             total = 0
         else:
             total += int(line)
 
+
+calories.sort(reverse=True)
+
 print("@@@@@@ OUTPUT @@@@@@")
-print(highest)
+print(calories[0] + calories[1] + calories[2])
